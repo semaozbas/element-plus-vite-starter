5b947a5090e3454b7cfee04ed1e0766be7d2e92b
semaozbas <sema.fahrulnisa.ozbas@gmail.com>
2023-10-30 06:51:21 +0300

    CSS is added

--- src/components/currencyConversion.vue
+++ src/components/currencyConversion.vue
@@ -1,25 +1,28 @@
 <template>
-   <div>
-      <div>
-         <h3>Currency Conversion</h3>
+   <div class="form-container">
+      <div class="title-container">
+         <h3 class="title">Currency Conversion</h3>
 
-         <div>
+         <div class="date-picker">
             <el-date-picker v-model="selectedDate" type="date" format="DD-MM-YYYY" placeholder="Pick a date" @change="updatestartDate()" />
          </div>
 
-         <div>
+         <div class="amount-currency">
             <el-input-number v-model="amount" :min="1" size="large" @change="handleChange" />
-            <el-select v-model="fromCurrencyCode" placeholder="Select from currency" center size="large" @change="fromCurrency">
+            <el-select class="select-container" v-model="fromCurrencyCode" placeholder="Select from currency" center size="large" @change="fromCurrency">
                <el-option v-for="currency in currencies" :value="currency" :key="currency" />
             </el-select>
          </div>
 
-         <div>
+         <div class="to-currency">
             <el-input-number v-model="convertedValue" disabled size="large" />
-            <el-select v-model="toCurrencyCode" placeholder="Select to currency" size="large" @change="toCurrency">
+            <el-select class="select-container" v-model="toCurrencyCode" placeholder="Select to currency" size="large" @change="toCurrency">
                <el-option v-for="currency in currencies" :value="currency" :key="currency" />
             </el-select>
          </div>
+
+         <p class="text" v-if="resultCurrency">{{ amount }} {{ fromCurrencyName }} equals</p>
+         <h1 v-if="resultCurrency">{{ convertedValue }} {{ toCurrencyName }}</h1>
       </div>
    </div>
 </template>
@@ -116,4 +119,50 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.title-container {
+   position: relative;
+
+   .title {
+      font-size: 26px;
+      color: #1114c9;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+   }
+}
+
+.date-picker {
+   margin-bottom: 20px;
+}
+
+.amount-currency {
+   align-items: center;
+}
+
+.to-currency {
+   align-items: center;
+}
+
+.text {
+   color: #0a0b0b;
+   font-size: 20px;
+   text-align: center;
+}
+
+.select-container {
+   padding: 6px 5px 6px 15px;
+   vertical-align: middle;
+   width: 200px;
+   display: inline-block;
+   margin: auto;
+}
+
+.form-container {
+   background-color: rgb(203, 206, 207);
+   width: 700px;
+   height: 450px;
+
+   position: relative;
+}
+</style>
